46668a0e6548ff83cce69ada60b56eeafe6f3bbf
Jae Huh <jaeeunhuh@gmail.com>
2017-09-10 23:13:51 +1200

    Complete

--- index.js
+++ index.js
@@ -48,22 +48,119 @@ async function getPostsByUser1() {
 // getPostsByUser1()
 
 // Get a list of titles
+async function getTitleList() {
+  const posts = await fetchData('https://jsonplaceholder.typicode.com/posts')
+  const postTitles = posts.map(post => post.title)
+  console.log(postTitles)
+}
+// getTitleList()
 
 // Get data from https://jsonplaceholder.typicode.com/users
 
 // Get list of all people who live in the cities of 'Wisokyburgh' or 'Roscoeview'
+async function getPeopleFromWR() {
+  const users = await fetchData('https://jsonplaceholder.typicode.com/users')
+  const filtered = users.filter(user => {
+    return user.address.city === 'Wisokyburgh' || user.address.city === 'Roscoeview'
+  })
+  console.log(filtered)
+}
+// getPeopleFromWR()
 
-// create a new array, that has all the posts but now includes the name and email of the user
+// Create a new array, that has all the posts but now includes the name and email of the user
+function getPostsWUserDetails() {
+  const getPosts = fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+  const getUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+  Promise.all([getPosts, getUsers])
+    .then(([postData, userData]) => {
+      const posts = postData
+      const users = userData
+      const detailsAdded = posts.map(post => {
+        const user = users.find(user => user.id === post.userId)
+        const userName = user.name
+        const userEmail = user.email
+        return {...post, userName, userEmail}
+      })
+      console.log(detailsAdded)
+    })
+}
+// getPostsWUserDetails()
 
-// get a list of posts by the person with the email 'incere@april.biz'
+// Get a list of posts by the person with the email 'Sincere@april.biz'
+function getPostList(userEmail) {
+  const getPosts = fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+  const getUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+  Promise.all([getPosts, getUsers])
+    .then(([postData, userData]) => {
+      const posts = postData
+      const users = userData
+      const userFoundWithEmail = users.find(user => user.email === userEmail)
+      const postList = posts.filter(post => post.userId === userFoundWithEmail.id)
+      console.log(postList)
+    })
+}
+// getPostList('Sincere@april.biz')
 
 // Get data from https://jsonplaceholder.typicode.com/todos
 
 // Count the number of completed todos
+async function countCompletedTodos() {
+  const todos = await fetchData('https://jsonplaceholder.typicode.com/todos')
+  const completed = todos.filter(todo => todo.completed)
+  console.log(completed.length)
+}
+// countCompletedTodos()
 
 // Create an object that has the completed todos and uncomplete todos { completed: [], uncompleted: [] }
+async function sortTodos() {
+  const todos = await fetchData('https://jsonplaceholder.typicode.com/todos')
+  const completed = todos.filter(todo => todo.completed)
+  const uncompleted = todos.filter(todo => !todo.completed)
+  console.log({completed, uncompleted})
+}
+// sortTodos()
 
 // Crate an object that has the users username, and then a object with their completed and incompleted todos
 // { Antonette: { completed: [], uncompleted: [] }
+function showUsersTodos() {
+  const getUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+  const getTodos = fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json())
+  Promise.all([getUsers, getTodos])
+    .then(([userData, todoData]) => {
+
+      const users = userData
+      const todos = todoData
+      const usersTodos = {}
+
+      for (const user of users) {
+        const userTodos = usersTodos[user.name.slice(0, user.name.indexOf(' '))] = {}
+
+        userTodos.completed = todos.find(todo => {
+          return todo.userId === user.id && todo.completed
+        })
+
+        userTodos.uncompleted = todos.find(todo => {
+          return todo.userId === user.id && !todo.completed
+        })
+      }
+
+      console.log(usersTodos)
+    })
+}
+// showUsersTodos()
 
 // Count the number of completed and uncompleted todos by the user who lives in the city 'Wisokyburgh'
+function countTodosOfUserFrom(city) {
+  const getUser = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+  const getTodos = fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json())
+  Promise.all([getUser, getTodos])
+    .then(([userData, todoData]) => {
+      const users = userData
+      const todos = todoData
+      const userFound = users.find(user => user.address.city === city)
+      const usersTodos = todos.filter(todo => todo.userId === userFound.id)
+      const completedTodos = usersTodos.filter(todo => todo.completed)
+      console.log(`Completed Todos: ${completedTodos.length}, Uncompleted Todos: ${usersTodos.length - completedTodos.length}`)
+    })
+}
+countTodosOfUserFrom('Wisokyburgh')
